b382f8df0b0f64d5f342809610d21294c81f0bb0
freaky4wrld <freaky4wrld@gmail.com>
2023-08-08 10:58:51 +0530

    Completed all the testcases in specs file and removed .skip() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    let result = ""
+    if (timesToRepeat>=0 && stringToRepeat!==''){
+        for (let i = 0; i<timesToRepeat; i++){
+            result+=stringToRepeat;
+        }
+        return result;
+    }
+    else if(stringToRepeat===''){return result;}
+    else{return "ERROR";}
 };
+console.log(repeatString('', 10));
 
 // Do not edit below this line
 module.exports = repeatString;
