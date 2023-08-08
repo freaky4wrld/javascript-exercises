8146a626e849761808d6d5a7ab801afada9ae7a2
freaky4wrld <freaky4wrld@gmail.com>
2023-08-08 11:18:35 +0530

    Challenge 3 completed along with all testcases unskipped and passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(toReversed) {
+    let result = ""
+    if(toReversed.length>0){
+        for (let i=toReversed.length-1; i>=0; i--){
+            result += toReversed[i];
+        }
+    }
+    return result;
 };
-
+console.log(reverseString(""));
 // Do not edit below this line
 module.exports = reverseString;
