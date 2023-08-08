37e45757288a5ba4af64ea6e009fc55557381f66
freaky4wrld <freaky4wrld@gmail.com>
2023-08-08 12:38:09 +0530

    Completed challenge with all testcase unskipped and passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    checkYear = parseInt(year);
+    if(checkYear%4===0 &&( checkYear%400===0||checkYear%100!==0)){
+        return true;
+    }
+    else{return false;}
 };
-
+console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
