fe6606b969e833811dff72eed58acb69e7c5590b
freaky4wrld <freaky4wrld@gmail.com>
2023-08-08 12:01:44 +0530

    Coompleted challenge 4 with all testcases unskipped

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayToModify, ...args) {
+    let elementToRemoved = [...args].sort();
+    arrayToModify = arrayToModify.sort();
+    for(let i=0; i<arrayToModify.length; i++){
+        for(let j=0; j<elementToRemoved.length; j++){
+            console.log(i,j)
+            if(arrayToModify[i]===elementToRemoved[j]){
+                arrayToModify.splice(i,1);
+            }
+   
+       }
+    }
+    return arrayToModify;
+    
 };
-
+console.log(removeFromArray([1,2,3,4,6],3,2));
 // Do not edit below this line
 module.exports = removeFromArray;
