873db6289ea34c7993db53e3369589b02c2989e4
freaky4wrld <freaky4wrld@gmail.com>
2023-08-08 12:16:16 +0530

    Completed Challenge 5 with all testcases unskipped

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumCount = 0;
+    let loopEnd = num1>num2?num1:num2;
+    let loopStart = num1<num2?num1:num2;
+    if(num1>=0 && num2>=0 && typeof(num1)==typeof(num2)){
+        for(let i=loopStart; i<=loopEnd; i++){
+            sumCount+=i;
+        }
+        return sumCount;
+    }
+    else{return "ERROR";}
 };
+
+console.log(sumAll(-1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
