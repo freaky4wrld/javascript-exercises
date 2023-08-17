9d54f4a17a32c066de3749bd91654007e94303f6
freaky4wrld <freaky4wrld@gmail.com>
2023-08-17 14:29:27 +0530

    found the oldest with all the testcases passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestIndex = 0;
+    let maxAge = 'yearOfDeath' in arr[0]?(arr[0].yearOfDeath - arr[0].yearOfBirth):(2023-arr[0].yearOfBirth);
+    for(let i=1; i<arr.length; i++){
+        let age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        if (maxAge<age){
+            maxAge = age;
+            oldestIndex = i;
+        }
+    }
+    return arr[oldestIndex];
 };
 
 // Do not edit below this line
