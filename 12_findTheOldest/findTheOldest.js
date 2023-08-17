f5e066b825262af5b320a8255bd62be32a1cd64e
freaky4wrld <freaky4wrld@gmail.com>
2023-08-17 14:30:48 +0530

    fixed the recent year problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,6 @@
 const findTheOldest = function(arr) {
     let oldestIndex = 0;
-    let maxAge = 'yearOfDeath' in arr[0]?(arr[0].yearOfDeath - arr[0].yearOfBirth):(2023-arr[0].yearOfBirth);
+    let maxAge = 'yearOfDeath' in arr[0]?(arr[0].yearOfDeath - arr[0].yearOfBirth):(new Date().getFullYear()-arr[0].yearOfBirth);
     for(let i=1; i<arr.length; i++){
         let age = arr[i].yearOfDeath - arr[i].yearOfBirth;
         if (maxAge<age){
