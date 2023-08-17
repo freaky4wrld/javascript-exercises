d87fb06179bd2321caacd352805d24b57c28193c
freaky4wrld <freaky4wrld@gmail.com>
2023-08-17 13:51:27 +0530

    completed the fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function fibo(num) {
+    num = parseInt(num);
+    console.log(num)
+    if (num>0){
+        f = [0, 1]
+        for (let i= 2; i< num+1; i++){          
+            f.push(f[i-1] + f[i-2])
+        }    
+        return f[num]
+    }else{
+        return 'OOPS'
+    }
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
